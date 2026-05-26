cb269918a01a1e127ed9f4aa074f035e2929e1b7
thgw <13313766+thgwalter@users.noreply.github.com>
2026-05-26 23:17:03 +0200

    Delete examples/applet directory

--- examples/applet/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject applet "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :aot [applet.core]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]])
--- examples/applet/test/applet/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns applet.test.core
-  (:use [applet.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
